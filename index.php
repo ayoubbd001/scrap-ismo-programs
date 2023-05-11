61240fa3ff0bc12fb4004b373c6c8c3b71694bc1
ayoubbd001 <123429064+ayoubbd001@users.noreply.github.com>
2023-05-11 15:05:17 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,398 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="shortcut icon"
+      href="https://ismo.ma/images/LogoOFPPTVF.png"
+      type="image/x-icon"
+    />
+    <meta
+      name="description"
+      content="ISMO is a leading educational institution offering a range of programs for college students. Our experienced faculty provides a comprehensive education in coding, design, and network fields to prepare you for success in the technology industry. Join ISMO and take the first step towards a fulfilling career in tech."
+    />
+    <link rel="stylesheet" href="assets/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+      integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <title>ISMO Tetouan</title>
+  </head>
+  <body>
+    <nav>
+      <div class="menu">
+        <a href="#home"><img src="assets/logo.png" alt="OFPPT ISMO" /></a>
+        <ul class="list">
+          <li><a href="#home" class="active">ACCUEIL</a></li>
+          <li><a href="#filieres">FILIÈRES</a></li>
+          <li><a href="#about">QUI SOMMES NOUS</a></li>
+          <li><a href="#emploi">EMPLOIS DU TEMPS</a></li>
+          <li><a href="#marquant">FAIT MARQUANT</a></li>
+          <li><a href="#addres">ADRESSE</a></li>
+        </ul>
+        <i class="fa-solid fa-bars"></i>
+      </div>
+    </nav>
+   
+    <header id="home">
+      <div class="curve">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#fff"
+            fill-opacity="1"
+            d="M0,192L60,202.7C120,213,240,235,360,240C480,245,600,235,720,208C840,181,960,139,1080,138.7C1200,139,1320,181,1380,202.7L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+      <div class="cover"></div>
+      <div class="header-content">
+        <h1>Institut spécialisé dans les métiers de l'offshoring Tétouan</h1>
+        <div class="links">
+          <a href="#home" class="active">Plus d'infos</a
+          ><a href="#home">Nous Contact</a>
+        </div>
+      </div>
+    </header>
+    <main>
+      <section id="filieres">
+        <h2>Les Filières</h2>
+        <div class="filieres-container">
+          <a href=""
+            ><div class="card"><h3>Développement Digital</h3></div></a
+          >
+          <a href="">
+            <div class="card"><h3>Infrastructure Digitale</h3></div></a
+          >
+          <a href="">
+            <div class="card"><h3>Infographie</h3></div></a
+          >
+          <a href="">
+            <div class="card">
+              <h3>Bureauticien Certifié en Microsoft office Spécialiste</h3>
+            </div></a
+          >
+          <a href="">
+            <div class="card">
+              <h3>Concepteur Réalisateur d'Application JEE</h3>
+            </div></a
+          >
+          <a href="">
+            <div class="card"><h3>Téléconseiller Centres d'Appels</h3></div></a
+          >
+        </div>
+      </section>
+      <section id="about">
+        <div class="curve2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#fff"
+              fill-opacity="1"
+              d="M0,256L60,229.3C120,203,240,149,360,133.3C480,117,600,139,720,154.7C840,171,960,181,1080,181.3C1200,181,1320,171,1380,165.3L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+            ></path>
+          </svg>
+        </div>
+        <div class="curve3">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#fff"
+              fill-opacity="1"
+              d="M0,288L60,261.3C120,235,240,181,360,186.7C480,192,600,256,720,277.3C840,299,960,277,1080,261.3C1200,245,1320,235,1380,229.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+            ></path>
+          </svg>
+        </div>
+        <div class="about-content">
+          <h2>Notre institut</h2>
+          <div class="about-container">
+            <p>
+              Cet institut, qui s'inscrit dans le cadre du programme de
+              développement intégré destiné à accompagner le Contrat-programme
+              TIC, mis en place par l'OFPPT, a pour mission de répondre aux
+              besoins en compétences des opérateurs économiques de la région,
+              d'offrir aux jeunes des qualifications adaptées au marché du
+              travail et de participer à la valorisation de la région du Nord.
+            </p>
+            <p>
+              D'une superficie de plus de 2.000 m2 couverts, l'ISMO a une
+              capacité d'accueil de 400 places et permet la formation de près de
+              700 stagiaires/an dans les niveaux techniciens spécialisés. Cet
+              établissement offre également des formations qualifiantes et les
+              cursus de formation dispensés couvrent l'ensemble des filières
+              liées aux métiers des DIGITAL.
+            </p>
+            <p>
+              L'institut forme dans les filières de développement digital,
+              infrastructures digital, et l'infographie pour le niveau
+              techniciens spécialisés, alors qu'une formation qualifiante
+              bureauticien certifié en microsoft office specialist est en
+              Téléconseiller Centres d'Appels sont également prévue en vue de
+              participer à la requalification des jeunes de Tétouan et sa région
+            </p>
+          </div>
+          <div class="presentation">
+            <div class="img"></div>
+            <span class="line"></span>
+            <div class="content">
+              <h3>Présentation de l'ISMO</h3>
+              <p>
+                Cet établissement comprend 6 salles spécialisées, 1 salle de
+                cours, une salle Centres d'Appels, une unité de ressources
+                didactiques en plus d'un bloc administratif, career center,
+                ainsi que d'autres équipements nécessaires au bon déroulement de
+                la formation.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="emploi">
+        <h2>Emplois du temps</h2>
+        <?php 
+          $output = shell_exec('python emplois/scrapDate.py');
+          echo '<span>' . $output  .' </span>';
+        ?>
+        <div class="emploi-container">
+          <div class="item"><a>CMOSW101</a></div>
+          <div class="item"><a>CRJ101</a></div>
+          <div class="item"><a>DEV101</a></div>
+          <div class="item"><a>DEV102</a></div>
+          <div class="item"><a>DEV103</a></div>
+          <div class="item"><a>DEVOWFS202</a></div>
+          <div class="item"><a>DEVOWFS203</a></div>
+          <div class="item"><a>ID101</a></div>
+          <div class="item"><a>ID102</a></div>
+          <div class="item"><a>ID103</a></div>
+          <div class="item"><a>IDOSR201</a></div>
+          <div class="item"><a>IDOSR202</a></div>
+          <div class="item"><a>IDOSR203</a></div>
+          <div class="item"><a>INFO101</a></div>
+          <div class="item"><a>INFO201</a></div>
+          <div class="item"><a>MIR201</a></div>
+          <div class="item"><a>MIR301</a></div>
+          <div class="item"><a>OPAO101</a></div>
+          <div class="item"><a>TCA101</a></div>
+          <div class="item"><a>TCI101</a></div>
+        </div>
+        <a href=""></a>
+      </section>
+      <section id="marquant">
+        <div class="cover"></div>
+        <div class="curve4">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#fff"
+              fill-opacity="1"
+              d="M0,160L60,138.7C120,117,240,75,360,69.3C480,64,600,96,720,101.3C840,107,960,85,1080,74.7C1200,64,1320,64,1380,64L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+            ></path>
+          </svg>
+        </div>
+        <div class="curve5">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#fff"
+              fill-opacity="1"
+              d="M0,64L48,101.3C96,139,192,213,288,240C384,267,480,245,576,213.3C672,181,768,139,864,149.3C960,160,1056,224,1152,218.7C1248,213,1344,139,1392,101.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            ></path>
+          </svg>
+        </div>
+        <div class="fait-content">
+          <h2>Fait Marquant</h2>
+          <div class="carousel">
+            <div class="slides">
+              <div class="slide">
+                <div class="text">
+                  <h3>City’Zen Challenge 2022</h3>
+                  <p>
+                    2021 : Les stagiaires de l’ISMO TETOUAN ont remporté le 2ème
+                    prix de City’Zen Challenge 2021. Le City’Zen Challenge de
+                    Berger-Levrault est un concours inter-écoles d’ingénieur qui
+                    donne la possibilité à des étudiants de 4 pays (La France,
+                    l’Espagne, Canada et pour la première fois cette année le
+                    Maroc avec la participation de l’ISMO TETOUAN et ENSA
+                    TETOUAN) d’être créateurs de la ville intelligente de demain
+                  </p>
+                </div>
+                <img src="" alt="" />
+              </div>
+              <div class="slide">
+                <div class="text">
+                  <h3>City’Zen Challenge 2022</h3>
+                  <p>
+                    2022 : Pour la deuxième année consécutive, une équipe de
+                    stagiaires de l'ISMO Tétouan a remporté le deuxième prix de
+                    City’Zen Challenge édition 2022 - la piste 2 du concours.
+                  </p>
+                </div>
+                <img src="" alt="" />
+              </div>
+              <div class="slide">
+                <div class="text">
+                  <h3>Google HashCode</h3>
+                  <p>
+                    2020: les quatre équipes qui ont participé au concours
+                    Google HashCode ont remporté les quatre premières places au
+                    niveau de Hub Everis Morocco
+                  </p>
+                </div>
+                <img src="" alt="" />
+              </div>
+            </div>
+            <button class="prev" aria-label="prev">
+              <i class="fa-solid fa-chevron-left"></i>
+            </button>
+            <button class="indecator" aria-label="indec">
+              <ul class="dots"></ul>
+            </button>
+            <button class="next" aria-label="next">
+              <i class="fa-solid fa-chevron-right"></i>
+            </button>
+          </div>
+        </div>
+      </section>
+      <section id="addres">
+        <div class="address">
+          <h2>Localisation</h2>
+          <iframe
+            title="local"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d25936.371014247314!2d-5.305364!3d35.651229!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0b5b615148fea3%3A0x37b6f3842f42dcbe!2sInstitut%20sp%C3%A9cialis%C3%A9%20dans%20les%20m%C3%A9tiers%20de%20l&#39;offshoring!5e0!3m2!1sfr!2sma!4v1679260170522!5m2!1sfr!2sma"
+            style="border: 0"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </section>
+    </main>
+    <footer>
+      <a href="">
+        <img src="https://ismo.ma/images/LogoOFPPTVF.png" alt=""
+      /></a>
+      <div class="contact">
+        <h3>Contact</h3>
+        <ul>
+          <li><a href="#home">Téléphone : +212 5397-07402</a></li>
+          <li><a href="#home">Email : infos@ismo.ma</a></li>
+        </ul>
+      </div>
+      <div class="Liens">
+        <h3>Liens</h3>
+        <ul>
+          <li><a href="#home">OFPPT</a></li>
+          <li><a href="#home">MYWAY</a></li>
+          <li><a href="#home">OFPPT Langues</a></li>
+          <li><a href="#home">WebForce</a></li>
+          <li><a href="#home">Scholarvox</a></li>
+        </ul>
+      </div>
+      <div class="madebyme">made with ❤ by saad</div>
+    </footer>
+    <script>
+      const menu = document.querySelector(".fa-solid");
+
+      menu.addEventListener("click", () => {
+        document.querySelector(".list").classList.toggle("active");
+        if (document.querySelector(".list").classList.contains("active")) {
+          menu.classList.remove("fa-bars");
+          menu.classList.add("fa-x");
+        } else {
+          menu.classList.add("fa-bars");
+          menu.classList.remove("fa-x");
+        }
+      });
+      Array.from(document.querySelectorAll(".list li a")).forEach((li) => {
+        li.addEventListener("click", () => {
+          document.querySelector(".list").classList.remove("active");
+          menu.classList.add("fa-bars");
+          menu.classList.remove("fa-x");
+        });
+      });
+      const nav = document.querySelector("nav");
+
+      window.addEventListener("scroll", () => {
+        let size = window.pageXOffset || document.documentElement.scrollTop;
+        if (size > 0) {
+          nav.style.top = "0%";
+        } else if (size == 0) {
+          nav.style.top = "25px";
+        }
+        end = size;
+      });
+
+      const nextbtn = document.querySelector(".next");
+      const prevbtn = document.querySelector(".prev");
+      const ul = document.querySelector(".dots");
+      const items = Array.from(document.querySelectorAll(".slide"));
+      const slideCount = items.length;
+      let currentIndex = 0;
+
+      nextbtn.addEventListener("click", nextSlide);
+      prevbtn.addEventListener("click", prevSlide);
+
+      for (let i = 1; i <= slideCount; i++) {
+        const li = document.createElement("li");
+        li.setAttribute("data_idex", i);
+        ul.appendChild(li);
+      }
+      function nextSlide() {
+        if (nextbtn.classList.contains("disabled")) {
+          return;
+        } else {
+          currentIndex++;
+          check();
+        }
+      }
+
+      function prevSlide() {
+        if (prevbtn.classList.contains("disabled")) {
+          return;
+        } else {
+          currentIndex--;
+          check();
+        }
+      }
+
+      function check() {
+        items.forEach((item) => {
+          item.classList.remove("active");
+        });
+        Array.from(ul.children).forEach((item) => {
+          item.classList.remove("active");
+        });
+
+        items[currentIndex].classList.add("active");
+        ul.children[currentIndex].classList.add("active");
+
+        if (currentIndex == 0) {
+          prevbtn.classList.add("disabled");
+        } else {
+          prevbtn.classList.remove("disabled");
+        }
+
+        if (currentIndex == slideCount - 1) {
+          nextbtn.classList.add("disabled");
+        } else {
+          nextbtn.classList.remove("disabled");
+        }
+      }
+      Array.from(ul.children).forEach((li) => {
+        li.addEventListener("click", () => {
+          let index = li.getAttribute("data_idex") - 1;
+          currentIndex = index;
+          check();
+        });
+      });
+      check();
+ 
+    </script>
+
+    <script src="assets/script.js"></script>
+  </body>
+</html>
