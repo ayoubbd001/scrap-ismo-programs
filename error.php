3a17d65075dd956d01a0a591f1655b462a22704a
ayoubbd001 <123429064+ayoubbd001@users.noreply.github.com>
2023-05-14 02:47:43 +0100

    Add files via upload

---
+++ error.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+           <!-- CSS only -->
+           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+           <!-- JavaScript Bundle with Popper -->
+           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <title>ERROR 404</title>
+</head>
+
+<body style="height:100vh;display:flex;align-items:center;">
+
+    
+
+    <div class="container py-10 my-lg-10">
+        <div class="row justify-content-center pt-lg-4 text-center">
+            <div class="col-lg-5 col-md-7 col-sm-9">
+                <h2 class="display-404 py-lg-3">404</h2>
+                <h3 class="mb-4">The link is broken or the page has been moved.</h3>
+                <p class="text-muted fs-md mb-4">
+                    Try these useful pages instead:
+                </p>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-xl-8 col-lg-10">
+                <ul class="list-inline text-center">
+                    <li class="list-inline-item"><a class="link-info link-hover-primary fs-lg" href="index.php">Home</a></li>
+                    <li class="list-inline-item"><a class="link-info link-hover-primary fs-lg" href="emplois.php">emplois</a></li>                            
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
